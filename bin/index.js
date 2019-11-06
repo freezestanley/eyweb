9e574cf4fa515e9f147aec33d8f810b4f266fd45
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2019-11-06 19:30:53 +0800

    xx

--- bin/index.js
+++ bin/index.js
@@ -1,38 +1,15 @@
-#!/usr/bin/env node
+#!/usr/bin/env node 
 const command = require('commander')
 const program = new command.Command()
-const chalk = require('')
-const path = require('path')
-const fs = require('fs')
-const pk = require(path.resolve(__dirname,'../package.json'))
 
 program
-  .version(pk.version)
-
+  .version('1.0.1')
 // 获取 help
 program.on('--help', function(){
-    console.log('')
-    console.log('Examples:');
-    console.log('  $ custom-help --help');
-    console.log('  $ custom-help -h');
-  })
+  console.log('')
+  console.log('Examples:');
+  console.log('  $ custom-help --help');
+  console.log('  $ custom-help -h');
+})
 
 program.parse(process.argv)
-
-program
-  .command('build')
-  .action((dir, otherDirs) => {
-    console.log('this is build')
-  })
-
-program
-  .command('dev')
-  .action((dir, otherDirs) => {
-    console.log('this is dev')
-  })
-
-program
-  .command('lint')
-  .action((dir, otherDirs) => {
-    console.log('this is lint')
-  })
