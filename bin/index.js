3c65012aa040be1fe21dc5c652a13892ae27fd3c
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2019-11-06 21:51:12 +0800

    xx

--- bin/index.js
+++ bin/index.js
@@ -15,21 +15,24 @@ program.on('--help', function(){
 })
 
 program
-  .command('serve', '启动开发服务')
+  .command('serve')
+  .description('启动开发服务')
   .action((e) => {
-    console.log('serve')
+    const child = spawn('cross-env', ['webpack-dev-server', '--hot', '--inline', '--colors', '--config', 'build/webpack.dev.conf.js'], { stdio: 'inherit' })
   })
 
 program
-  .command('build', '构建生产版本')
+  .command('build')
+  .description('构建生产版本')
   .action((e) => {
-    console.log('build')
+    const child = spawn('cross-env', ['NODE_ENV=production', 'webpack', '--progress', '--colors', '--config', 'build/webpack.product.conf.js'], { stdio: 'inherit' })
   })
 
 program
-  .command('lint', '静态扫描')
+  .command('lint')
+  .description('静态扫描')
   .action((e) => {
-    console.log('lint')
+    const child = spawn('eslint', ['--ext', '.jsx,.js', 'src', '-f', 'checkstyle', '-o', 'report_zacc_eslint_js.xml;', 'exit', '0'], { stdio: 'inherit' })
   })
 
 program.parse(process.argv)
